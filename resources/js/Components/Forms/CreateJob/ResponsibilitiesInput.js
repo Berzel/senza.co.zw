2638f5abddf0395312cd9bc1f39ab6b194849433
Berzel Best <berzel@classifieds.co.zw>
2022-06-20 16:57:38 +0200

    Auto height text area

--- resources/js/Components/Forms/CreateJob/ResponsibilitiesInput.js
+++ resources/js/Components/Forms/CreateJob/ResponsibilitiesInput.js
@@ -4,9 +4,24 @@ import PlusIcon from "../../Icons/PlusIcon"
 function Responsibility({responsibility, position, setResponsibility, removeResponsibity}) {
     const [focused, setFocused] = useState(false)
 
+    function adjustHeight (domEl) {
+        domEl.style.height = 'auto'
+        domEl.style.height = `${domEl.scrollHeight}px`
+    }
+
     useEffect(() => {
-        focused && document.getElementById(`responsibility-${position}`).focus()
+        if (focused) {
+            const textarea = document.getElementById(`responsibility-${position}`)
+            adjustHeight(textarea)
+            textarea.focus()
+        }
     }, [focused])
+
+    function handleChange (e) {
+        const textarea = e.target
+        setResponsibility(textarea.value.replace(/ {2,}|\r\n|\n|\r/gm, ' '))
+        adjustHeight(textarea)
+    }
 
     return (
         <li className="flex items-start">
@@ -17,14 +32,15 @@ function Responsibility({responsibility, position, setResponsibility, removeResp
             <div className={`${focused ? 'flex-grow' : ''}`}>
                 {
                     focused ? (
-                        <input
-                            type="text"
+                        <textarea
+                            rows="1"
                             value={responsibility}
+                            onChange={handleChange}
                             onBlur={() => setFocused(false)}
                             id={`responsibility-${position}`}
-                            className="ml-2 block outline-none w-[90%]"
-                            onChange={e => setResponsibility(e.target.value)}
                             placeholder={`Edit responsibility #${position + 1}`}
+                            className="ml-2 block w-full outline-none transition bg-transparent"
+                            style={{msOverflowStyle: 'none',scrollbarWidth: 'none',overflowY: 'scroll'}}
                         />
                     ) : (
                         <span className="ml-2 block cursor-pointer" onClick={() => setFocused(true)}>
@@ -34,32 +50,28 @@ function Responsibility({responsibility, position, setResponsibility, removeResp
                 }
             </div>
 
-            {
-                !focused && (
-                    <div className="flex items-center ml-6 space-x-2">
-                        <button
-                            type="button"
-                            onClick={() => setFocused(true)}
-                            title={`Edit responsibility #${position+1}`}
-                            className="w-5 h-5 rounded-full text-sky-600 hover:text-pink-600 bg-sky-50 hover:bg-pink-50 flex items-center justify-center transition hover:scale-125"
-                        >
-                            <svg className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                            </svg>
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => removeResponsibity(position)}
-                            title={`Remove responsibility #${position+1}`}
-                            className="w-5 h-5 rounded-full text-pink-600 hover:text-sky-600 bg-pink-50 hover:bg-sky-50 flex items-center justify-center transition hover:scale-125"
-                        >
-                            <svg className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
-                )
-            }
+            <div className="flex items-center ml-6 space-x-2">
+                <button
+                    type="button"
+                    onClick={() => setFocused(true)}
+                    title={`Edit responsibility #${position+1}`}
+                    className="w-5 h-5 rounded-full text-sky-600 hover:text-pink-600 bg-sky-50 hover:bg-pink-50 flex items-center justify-center transition hover:scale-125"
+                >
+                    <svg className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                    </svg>
+                </button>
+                <button
+                    type="button"
+                    onClick={() => removeResponsibity(position)}
+                    title={`Remove responsibility #${position+1}`}
+                    className="w-5 h-5 rounded-full text-pink-600 hover:text-sky-600 bg-pink-50 hover:bg-sky-50 flex items-center justify-center transition hover:scale-125"
+                >
+                    <svg className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                    </svg>
+                </button>
+            </div>
         </li>
     )
 }
